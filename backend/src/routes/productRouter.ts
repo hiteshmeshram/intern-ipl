4fc9817c28da77ef989047770654587c5af8aaf7
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-12-03 13:04:38 +0530

    added product routes

---
+++ backend/src/routes/productRouter.ts
@@ -0,0 +1,69 @@
+
+import { Router } from 'express'
+import { adminMiddleware } from '../middleware/adminMiddleware';
+import { client } from '..';
+
+export const productRouter = Router();
+
+productRouter.post('/add',adminMiddleware,async (req,res)=>{
+    const {name,description,price,image_url} = req.body;
+    try {
+        await client.product.create({
+            data: {
+                name,
+                description,
+                price,
+                image_url
+            }
+        })
+
+        res.json({
+            message: 'product added successfully'
+        })
+
+    } catch(e) {
+        console.log(e);
+        res.status(403).json({
+            message: 'error while adding product'
+        })
+    }
+})
+
+productRouter.put('/edit',adminMiddleware,async (req,res)=>{
+    const {id,data} = req.body;
+
+    try {
+        await client.product.update({
+            where: {
+                id: req.body.id
+            },
+            data: req.body.data
+        })
+
+        res.json({
+            message: 'product updated successfully'
+        })
+
+    } catch(e) {
+        console.log(e);
+        res.status(403).json({
+            message: 'error while editing the product'
+        })
+    }
+})
+
+productRouter.delete('/delete',adminMiddleware,async (req,res)=>{
+    const {id } = req.body;
+    try {
+        await client.product.delete({
+            where: {
+                id
+            }
+        })
+    } catch(e) {
+        console.log(e);
+        res.status(404).json({
+            message: 'error while deleting'
+        })
+    }
+})
